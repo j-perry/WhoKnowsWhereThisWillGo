1cb82ae145bac44f87959b969a0adf977b16a02b
Jonathan Perry <j.m.perry@hotmail.co.uk>
2014-09-29 11:33:58 +0100

    Added program title

--- WhoKnowsWhereThisWillGo/main.cpp
+++ WhoKnowsWhereThisWillGo/main.cpp
@@ -3,8 +3,11 @@
 
 int main(int argc, char *argv[])
 {
+    char *title = "Who Knows Where This Will Go";
     QApplication a(argc, argv);
     MainWindow w;
+
+    w.setWindowTitle(title);
     w.show();
 
     return a.exec();
